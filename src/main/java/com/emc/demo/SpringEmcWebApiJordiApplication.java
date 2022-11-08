995e5de1766320bb86e6e8adfe28ebdc6eb986db
jordiascension <jordiascension@enterpriseconsulting.es>
2022-11-08 12:57:16 +0100

    Added first commit with springboot Example

---
+++ src/main/java/com/emc/demo/SpringEmcWebApiJordiApplication.java
@@ -0,0 +1,13 @@
+package com.emc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmcWebApiJordiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmcWebApiJordiApplication.class, args);
+	}
+
+}
