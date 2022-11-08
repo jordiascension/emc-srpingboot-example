81321f8202aeba8a4f93265c9690812514228e94
jordiascension <jordiascension@enterpriseconsulting.es>
2022-11-08 16:53:16 +0100

    Added GetStudents Rest Api with Mysql

---
+++ sql/V1__Script.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `heroku_e64aed2083389e0`.`student` (
+  `idstudent` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `surname` VARCHAR(100) NULL,
+  `age` INT NULL,
+  PRIMARY KEY (`idstudent`))
+COMMENT = '		';
--- src/main/java/com/emc/demo/SpringEmcWebApiJordiApplication.java
+++ src/main/java/com/emc/demo/SpringEmcWebApiJordiApplication.java
@@ -1,13 +1,34 @@
 package com.emc.demo;
 
+import java.awt.Desktop;
+import java.net.URI;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SpringEmcWebApiJordiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEmcWebApiJordiApplication.class, args);
+	}
+
+	@EventListener({ ApplicationReadyEvent.class })
+	void applicationReadyEvent() {
+		System.out.println("Application started ... launching browser now");
+		browse("http://localhost:8080/HelloWorld/hello");
+	}
+
+	public static void browse(String url) {
+		System.setProperty("java.awt.headless", "false");
+		Desktop desktop1 = Desktop.getDesktop();
+		try {
+			desktop1.browse(new URI(url));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
